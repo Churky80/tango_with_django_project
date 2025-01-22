a9147b824c0690352562387f10d2c0e802e18125
Churky80 <2992930M@student.gla.ac.uk>
2025-01-22 12:52:38 +0000

    Completed Exercise: added about view and URL mappings

--- rango/views.py
+++ rango/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def about(request):
+    return render(request, 'rango/about.html')
+
+def index(request):
+    return render(request, 'rango/index.html')
+
