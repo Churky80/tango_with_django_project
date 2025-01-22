5e61f869677682dd78c74163ab37809c5cc84685
Churky80 <2992930M@student.gla.ac.uk>
2025-01-22 13:35:32 +0000

    Updated about.html with dynamic content and base.html template

--- rango/views.py
+++ rango/views.py
@@ -2,7 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def about(request):
-    return render(request, 'rango/about.html')
+    context_dict = {'boldmessage': 'This tutorial has been put together by Jiahui'}
+    return render(request, 'rango/about.html', context=context_dict)
 
 def index(request):
     return render(request, 'rango/index.html')
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('rango/', include('rango.urls')),
 ]
+
+# 配置媒体文件的URL模式（仅在开发模式下）
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
